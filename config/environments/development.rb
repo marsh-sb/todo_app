d38bbad32037f2275a16b74e1daef9d226370f8f
marsh_sb <enotaku5526@gmail.com>
2021-03-20 14:23:38 +0000

    add react & webpacker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "549719dedf5940c49430095ad934a3aa.vfs.cloud9.us-east-1.amazonaws.com"
 end
